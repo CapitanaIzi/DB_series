6101a9297b3d45e9f09f4a7519e3590760b0dcad
PC 2 <>
2024-07-04 20:24:12 -0300

    jueves

--- comandos.sql
+++ comandos.sql
@@ -19,27 +19,29 @@ SHOW DATABASES;
 SHOW TABLES;
 SELECT * FROM donghuas;
 
+-- para eliminar la tabla
+DROP TABLE datos_adicionales; 
 CREATE TABLE datos_adicionales (
     id INT PRIMARY KEY AUTO_INCREMENT,
     donghua_id INT,
     vistas FLOAT NOT NULL,
     temporales_totales INT NOT NULL,
-    adaptacion VARCHAR(20),
+    adaptacion TINYINT(1) DEFAULT 0,
     subtitulos TINYINT(1) DEFAULT 0,
     FOREIGN KEY (donghua_id) REFERENCES donghuas(id) 
 );
 
 
-INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(1,18656570,2,"Novela",1);
-INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(2,13176969,1,"Novela",1);
-INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(3,2581508,1,"Novela ligera",1);
-INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(4,158077,4,"Manhua",1);
-INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion)VALUES(5,850971,2,"Novela");
+INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(1,18656570,2,1,1);
+INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(2,13176969,1,1,1);
+INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(3,2581508,1,2,1);
+INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion,subtitulos)VALUES(4,158077,4,3,1);
+INSERT INTO datos_adicionales (donghua_id,vistas,temporales_totales,adaptacion)VALUES(5,850971,2,1);
 
 SELECT * FROM datos_adicionales;
 
 SELECT * FROM donghuas JOIN datos_adicionales ON id=donghua_id;
 
-DROP TABLE datos_adicionales; -- para eliminar la tabla
-
     -- FOREIGN KEY (donghua_id) REFERENCES donghuas(id)  es para relacionar el id de una tabla y la otra
+
+    
--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
 # documentacion 
+## donghuas tabla
+1. Estado
+    - 0: Finalizado
+    - 1: En emision
 ## datos_adicionales tabla
-1. Subtitulos 
+1. Adaptacion
+    - 0: No disponible
+    - 1: Novela
+    - 2: Novela ligera
+    - 3: Manhua
+2. Subtitulos 
     - 0 : No disponible
     - 1 : Español
